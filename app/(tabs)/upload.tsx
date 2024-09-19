f35b870ac6ed44ac7e27ea008613679e3cd85679
Fardeen <fardeenclan@gmail.com>
2024-09-19 23:39:35 +0530

    init

--- app/(tabs)/upload.tsx
+++ app/(tabs)/upload.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { View, Text, StyleSheet, Button, Image } from "react-native";
+import { StyleSheet, Button, Image, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import * as FileSystem from "expo-file-system";
 import * as ImagePicker from "expo-image-picker";
 const Upload = () => {
   const [image, setImage] = useState<string | null>(null);
-
+  const [reviewResult, setReviewResult] = useState<string | null>(null);
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
@@ -13,16 +14,56 @@ const Upload = () => {
       quality: 1,
     });
 
-    console.log(result);
-
     if (!result.canceled) {
       setImage(result.assets[0].uri);
+    }
+  };
+  const getReview = async () => {
+    if (!image) {
+      alert("Please select a video first");
+      return;
+    }
+
+    const fileInfo = await FileSystem.getInfoAsync(image);
+    if (!fileInfo.exists) {
+      alert("File does not exist");
+      return;
+    }
+    const formData = new FormData();
+    formData.append("image", {
+      uri: image,
+      type: "image/jpeg",
+      name: "image.jpg",
+    } as any);
+    try {
+      const response = await fetch("http://127.0.0.1:5000/upload-image", {
+        method: "POST",
+        body: formData,
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.error("Server error:", errorData);
+        alert("Error uploading video: " + errorData.Error);
+        return;
+      }
+
+      const data = await response.json();
+      setReviewResult(data.Percentage);
+    } catch (error) {
+      console.error("Error uploading video:", error);
+      alert("Error uploading video");
     }
   };
   return (
     <SafeAreaView style={styles.container}>
       <Button title="Pick an image from camera roll" onPress={pickImage} />
       {image && <Image source={{ uri: image }} style={styles.image} />}
+      <Button title="Submit" onPress={getReview} />
+      <Text>{reviewResult}</Text>
     </SafeAreaView>
   );
 };
--- components/MapView.tsx
+++ components/MapView.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView style={styles.map} provider="google" />
     </View>
   );
 }
