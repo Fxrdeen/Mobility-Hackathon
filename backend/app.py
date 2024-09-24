6dace2a2b9488e7b8d5016ce59da2bc0b34650d8
Fardeen <fardeenclan@gmail.com>
2024-09-25 00:02:26 +0530

    added supabase inserting and fetching

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,21 @@
-from flask import Flask,request,jsonify
-from flask_restful import Api,Resource
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource
 from roboflow import Roboflow
 import supervision as sv
 import cv2
 import os
 import numpy as np
+
 app = Flask(__name__)
 api = Api(app)
-# CORS(app)
+
+# Ensure the 'uploads' directory exists
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'backend', 'uploads')
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 class FootPath(Resource):
     def post(self):
         try:
@@ -15,7 +23,7 @@ class FootPath(Resource):
                 return jsonify({'Error': 'Image not received'})
 
             image = request.files['image']
-            image_path = os.path.join('backend','uploads', image.filename)
+            image_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
             print(image_path)
             image.save(image_path)
 
@@ -47,8 +55,7 @@ class FootPath(Resource):
             return jsonify({'Error': str(e)})
 
 
-api.add_resource(FootPath,'/upload-image')
+api.add_resource(FootPath, '/upload-image')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
